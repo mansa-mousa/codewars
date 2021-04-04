37bc7b44ce31da284831dbecc5d1d56e8de91b6e
Mansa <someel@gmail.com>
2021-04-04 21:55:19 +0200

    findSum

--- main.js
+++ main.js
@@ -61,3 +61,13 @@ function filter_list(l) {
     return str.endsWith(ending)
    }
    
+  //  Sum of all the multiples of 3 or 5
+  function findSum(n) {
+    let sum = 0
+    for(let i = 0; i <= n; i++){
+      if((i % 3 == 0) || (i % 5 == 0)){
+        sum += i
+    }
+    }
+   return sum
+  }
