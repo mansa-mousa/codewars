67f82524a58337c729985610e46524a424a6852a
Mansa <someel@gmail.com>
2021-05-01 23:47:02 +0200

    Swap Values

--- main.js
+++ main.js
@@ -397,3 +397,11 @@ class Kata {
     return length * width * height
   }
 }
+
+// Swap Values
+function swapValues(arr) {
+  let temp = arr[0]
+    arr[0] = arr[1]
+    arr[1] = temp
+  return arr
+}
