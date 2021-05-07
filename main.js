1b7f794ab30c4586a28b07647d50d92b9c48906d
Mansa <someel@gmail.com>
2021-05-07 11:44:26 +0200

    Remove First and Last Character Part Two

--- main.js
+++ main.js
@@ -439,3 +439,8 @@ function arrayMadness(a, b) {
 function check(a,x){
   return a.includes(x)
 };
+
+// Remove First and Last Character Part Two
+function array(arr){
+  return arr.split(',').slice(1, -1).join(' ') || null
+}
