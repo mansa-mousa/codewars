65a1c4b2258c62beb905e8fdb39baf3b0a41d378
Mansa <someel@gmail.com>
2021-05-08 12:17:00 +0200

    Find the Difference in Age between Oldest and Youngest Family Members

--- main.js
+++ main.js
@@ -471,3 +471,13 @@ function squareOrSquareRoot(array) {
   })
   return newArray
 }
+
+// Find the Difference in Age between Oldest and Youngest Family Members
+function differenceInAges(ages){
+  ages.sort((a,b)=>{
+    return a - b
+  })
+  let newArray = []
+  newArray.push(ages[0], ages[ages.length-1], ages[ages.length-1]-ages[0])
+  return newArray
+}
